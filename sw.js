c3912fb4fa87afbc1dcf2f9a3323ce09c38cda92
gisolput-sketch <gisolput@gmail.com>
2026-09-20 14:33:55 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,15 @@
+const CACHE='22-drive-v1';
+const APP_SHELL=['./','./index.html','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png'];
+self.addEventListener('install',event=>{
+  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(APP_SHELL)).then(()=>self.skipWaiting()));
+});
+self.addEventListener('activate',event=>{
+  event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));
+});
+self.addEventListener('fetch',event=>{
+  if(event.request.method!=='GET') return;
+  const url=new URL(event.request.url);
+  if(url.origin===self.location.origin){
+    event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request)));
+  }
+});
